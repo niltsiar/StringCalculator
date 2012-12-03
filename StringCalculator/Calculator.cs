01f09a7b405ebb90b2c9f82dbc34e7b82d8dfdbd
Borja Quevedo <borjota@gmail.com>
2012-12-03 21:32:14 +0100

    Punto 4 de la kata resuelto

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        private bool _customDelimiter;
+        private char[] _delimiters;
+
+        public Calculator()
+        {
+            _delimiters = new[] {',', '\n'};
+        }
+
+        public int Add(string input)
+        {
+            var stringNumbers = ProcessInput(input);
+
+            var digits = ConvertToNumbers(stringNumbers);
+
+            return digits.Sum();
+        }
+
+        private IEnumerable<int> ConvertToNumbers(IEnumerable<string> numbers)
+        {
+            return from c in numbers select Convert.ToInt32(c);
+        }
+
+        private bool CheckTwoDelimiterTogether(string numbers)
+        {
+            var sb = new StringBuilder();
+            Array.ForEach(_delimiters, x => sb.Append(x));
+            var regex = @"\d[" + sb + "]{2}";
+            return Regex.IsMatch(numbers, regex);
+        }
+
+        private bool ExtractDelimiters(string input)
+        {
+            _customDelimiter = input.StartsWith("//");
+
+            if (_customDelimiter)
+                _delimiters = new[] { input[2]};
+
+            return _customDelimiter;
+        }
+
+        private IEnumerable<string> SplitNumbers(string numbers)
+        {
+            if (_customDelimiter)
+            {
+                foreach (var delimiter in _delimiters)
+                {
+                    if (!numbers.Contains(delimiter)) throw new DelimiterException();
+                }
+            }
+
+            if (CheckTwoDelimiterTogether(numbers)) throw new DelimiterException();
+
+            return numbers.Split(_delimiters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private IEnumerable<string> ProcessInput(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return new[] { "0" };
+
+            if (ExtractDelimiters(input))
+                input = input.Substring(4);
+
+            return SplitNumbers(input);
+        }
+    }
+}
---
+++ StringCalculatorTest/CalculatorTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace StringCalculatorTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test(Description = "Sumar cadena vacía")]
+        public void ItAddsEmptyString()
+        {
+            var result = _calculator.Add(string.Empty);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test(Description = "Realizar la suma con un número")]
+        public void ItAddsOneNumber()
+        {
+            var result = _calculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test(Description = "Sumar un caracter no numérico")]
+        public void ItThrowsAnExceptionWhenOneNonNumericalCharacterAsEntry(
+            [Values("a", "b", "ñ", "A", "?", "@")] string character)
+        {
+            Assert.Throws<FormatException>(() => _calculator.Add(character));
+        }
+
+        [Test(Description = "Sumar dos números separados por ,")]
+        public void ItAddsTwoNumbers()
+        {
+            var result = _calculator.Add("1,2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test(Description = "Sumar multiples con caracteres raros separados por ,")]
+        public void ItThrowsAnExceptionWhenTwoNonNumericalCharacterAsEntry(
+            [Values("a", "b", "ñ", "A", "?", "@")] string character1,
+            [Values("a", "b", "ñ", "A", "?", "@")] string character2)
+        {
+            Assert.Throws<FormatException>(() => _calculator.Add(character1 + ',' + character2));
+        }
+
+        [Test(Description = "Sumar varios números separados por ,")]
+        public void ItAddsMultipleNumbers()
+        {
+            var result = _calculator.Add("1,2,3,4,5,6,7,8");
+
+            Assert.AreEqual(36, result);
+        }
+
+        [Test(Description = "Sumar dos números con delimitador , y \n")]
+        public void ItAddsTwoNumersSeparatedByCommaOrCrLf()
+        {
+            var result = _calculator.Add("1\n2,3");
+
+            Assert.AreEqual(6, result);
+        }
+
+        [Test(Description = "Entrada errónea dos separadores juntos")]
+        public void ItThowsAnExceptionWithTwoSeparatorsTogether(
+            [Values(',', '\n')] char delimiter1,
+            [Values(',', '\n')] char delimiter2)
+        {
+            Assert.Throws<DelimiterException>(() => _calculator.Add("1" + delimiter1 + delimiter2));
+        }
+
+        [Test(Description = "Entrada con delimitador al principio")]
+        public void ItAddTwoNumbersWithInitialDelimiter()
+        {
+            var result = _calculator.Add("//;\n1;2");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test(Description = "Usar un delimitador distinto al declarado al inicio"), Sequential]
+        public void ItThrowsAnExceptionWhenUseOneDelimiterDifferentFromDeclaredInitially(
+            [Values(';', ',')] char delimiter1,
+            [Values(',', ';')] char delimiter2)
+        {
+            var expression = "//" + delimiter1 + "\n1" + delimiter2 + "2";
+            Assert.Throws<DelimiterException>(() => _calculator.Add(expression));
+        }
+    }
+}
